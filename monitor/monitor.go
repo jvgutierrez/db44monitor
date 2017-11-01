3bd7b8e80700c66ef66b72fb3821015d324f5f73
Valentín Gutiérrez <valentin.gutierrez@schibsted.com>
2017-11-01 18:03:21 +0100

    Sort measures before combining with GPX file

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -10,6 +10,12 @@ type Measure struct {
 	RfLevel   float64   `json:"rf_level"`
 }
 
+type ByTimestamp []Measure
+
+func (p ByTimestamp) Len() int           { return len(p) }
+func (p ByTimestamp) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p ByTimestamp) Less(i, j int) bool { return p[i].Timestamp.Before(p[j].Timestamp) }
+
 type MonitorManager interface {
 	Measures() <-chan Measure
 	Close()
